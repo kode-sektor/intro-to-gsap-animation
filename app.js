d2a84ea3bcb6d1b3346539b1bf6a09a62c1b57e9
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-10-20 03:02:26 -0400

    Annotate code

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const tl = gsap.timeline({defaults: {ease : 'power1.out'}})
 
-tl.to('.text', {y: '0%', duration: 1, stagger: 0.25})  // Move up. Dtagger: line-by-line movement instead of as one contiguous block
+// Move up. Dtagger: line-by-line movement instead of as one contiguous block
+tl.to('.text', {y: '0%', duration: 1, stagger: 0.25}) 
+
 tl.to('.slider', {y : '-100%', duration: 1.5, delay: 1.5})
+
 tl.to('.intro', {y: '-100%', duration: 1}, "-=1") // Start 1 sec faster
 
-tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1})// Start from 0 opacity to 1 for nav
+// Start from 0 opacity to 1 for nav
+tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1})
+
+// Start 1 sec faster which annuls the 1-sec duration of the previous
+// meaning it starts at the same time
 tl.fromTo('.bigText', {opacity: 0}, {opacity: 1, duration: 1}, '-=1')
+
